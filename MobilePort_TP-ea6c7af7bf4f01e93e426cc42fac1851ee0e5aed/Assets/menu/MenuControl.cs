137267968057f3bd4cf0ef665824f43008ea7257
martin <martinconcetti97@gmail.com>
2020-12-03 02:26:39 -0300

    Menu and credits

---
+++ MobilePort_TP-ea6c7af7bf4f01e93e426cc42fac1851ee0e5aed/Assets/menu/MenuControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControl : MonoBehaviour
+{
+
+    public void Start()
+    {
+
+    }
+
+    public void Play()
+    {
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene("conduccion9");
+
+    }
+
+    public void Credits()
+    {
+
+    }
+
+    public void Back()
+    {
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene("menu");
+
+    }
+
+    public void Quit()
+    {
+
+        Application.Quit();
+    }
+}
